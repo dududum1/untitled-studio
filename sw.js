3ab1c1a804f7ed62582e1407a9feac939c31148d
dududum1 <damarmahesa79@gmail.com>
2026-02-21 04:38:28 +0700

    chore: bump service worker cache to v19

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * v2: Added Phase 2 modules and placeholder assets
  */
 
-const CACHE_NAME = 'untitled-studio-v18';
+const CACHE_NAME = 'untitled-studio-v19';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
